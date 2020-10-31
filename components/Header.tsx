629dc727ff7cfe458e7a5850ef3ddd3222b3987d
David <davidmcnamee314@gmail.com>
2020-10-31 17:01:09 -0400

    menu and header bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,93 @@
 import Link from "next/link";
+import { useState } from "react";
+import styled from 'styled-components';
+import { COLORS } from '../utils/colors';
 
 export const Header = () => {
+  const [showMenu, setMenu] = useState(false);
+  const toggleMenu = () => setMenu(!showMenu);
+
   return (
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{" "}
-        |{" "}
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </nav>
-    </header>
+    <HeaderBackground>
+      <MenuContainer showMenu={showMenu}>
+        <ul>
+          <li>
+            <strong>About Me</strong>
+            <ul>
+              <Link href="/resume.pdf"><li>Resume</li></Link>
+              <Link href="https://github.com/leon-li1"><a><li>Github</li></a></Link>
+              <Link href="https://www.linkedin.com/in/leon-li1/"><a><li>Linkedin</li></a></Link>
+            </ul>
+          </li>
+          <li>
+          <strong>Projects</strong>
+            <ul>
+              <a href="#urban-lyrics"><li>Urban Lyrics<NewBadge /></li></a>
+              <a href="#data-rush"><li>Data Rush</li></a>
+              <a href="#frogger"><li>Frogger</li></a>
+            </ul>
+          </li>
+        </ul>
+      </MenuContainer>
+      <SandwichIcon onClick={toggleMenu} src="/sandwich-menu.svg"/>
+      <img height="45%" src="/site-logo.svg"/>
+    </HeaderBackground>
   );
 };
+
+const SandwichIcon = styled.img`
+  height: 30%;
+  cursor: pointer;
+`;
+
+const HeaderBackground = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 15px;
+  position: fixed;
+  height: 60px;
+  width: 100vw;
+  background-color: ${COLORS.black}80;
+`;
+
+type MenuContainerProps = {
+  showMenu: boolean;
+};
+
+const MenuContainer = styled.div<MenuContainerProps>`
+  position: fixed;
+  top: 60px;
+  left: ${({ showMenu }) => showMenu ? '0px' : '-300px'};
+  background-color: ${COLORS.darkBlue};
+  width: 300px;
+  transition: left 0.5s ease-in-out;
+  color: white;
+  font-size: 1.2em;
+  li {
+    list-style-type: none;
+    padding: 0.4em 0;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+  }
+`;
+
+const NewBadge = () => {
+  return <NewBadgeContainer>
+    <strong>New!</strong>
+  </NewBadgeContainer>
+};
+
+const NewBadgeContainer = styled.div`
+  display: inline-block;
+  background-color: white;
+  border-radius: 12px;
+  vertical-align: top;
+  font-size: 0.3em;
+  color: ${COLORS.darkBlue};
+  padding: 0 0.8em;
+  margin: 1em;
+`;
---
+++ utils/colors.ts
@@ -0,0 +1,8 @@
+export const COLORS = { 
+  darkBlue: '#0d0e4e',
+  turquoise: '#34d1bf',
+  lightGreen: '#76e374',
+  mediumBlue: '#2224aa',
+  black: '#000000',
+};
+
