8b101d50c2257d743d3d2ca1787198432314ca21
David <davidmcnamee314@gmail.com>
2020-11-01 01:46:56 -0400

    favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,6 +14,7 @@ const Layout = ({ children, title = "This is the default title" }: Props) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico?v=2" />
     </Head>
     <Header />
     {children}
