23f800482f07c80efbe79fb7557b2da5a4a5144b
Leon Li <leonli369@gmail.com>
2020-11-01 21:18:11 -0500

    updated froggerCard colour

--- components/IndexPage/FroggerCard.tsx
+++ components/IndexPage/FroggerCard.tsx
@@ -3,11 +3,7 @@ import { COLORS, SMALL } from "../../utils/constants";
 import { Card } from "../Card";
 
 export const FroggerCard = () => (
-  <Card
-    backgroundColor={COLORS.mediumBlue}
-    offset={{ x: 120, y: 0 }}
-    id="frogger"
-  >
+  <Card backgroundColor={COLORS.blue} offset={{ x: 120, y: 0 }} id="frogger">
     <Heading>Frogger</Heading>
     <HorziontalContainer>
       <Img />
